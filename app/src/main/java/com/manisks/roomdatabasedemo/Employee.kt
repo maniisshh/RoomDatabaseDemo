47aa56daf659c013d71473166f41aafe13d836fa
Manish <manish20393@gmail.com>
2019-09-13 18:09:30 +0530

    Created Structure

---
+++ app/src/main/java/com/manisks/roomdatabasedemo/Employee.kt
@@ -0,0 +1,11 @@
+package com.manisks.roomdatabasedemo
+
+import androidx.room.Entity
+
+/**
+ * Created by user on 12-09-2019.
+ */
+
+@Entity(tableName = "employee")
+class Employee {
+}
---
+++ app/src/main/java/com/manisks/roomdatabasedemo/EmployeeDatabase.kt
@@ -0,0 +1,7 @@
+package com.manisks.roomdatabasedemo
+
+/**
+ * Created by user on 12-09-2019.
+ */
+abstract class EmployeeDatabase {
+}
